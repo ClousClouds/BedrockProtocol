c69ac42ed3a945cf711b9d6adb11b6f7efb6dcaf
AzliRynz <nurazligaming@gmail.com>
2026-08-08 19:10:28 +0700

    fix: update ResourcePackClientResponse packet decoding

--- src/ResourcePackClientResponsePacket.php
+++ src/ResourcePackClientResponsePacket.php
@@ -16,10 +16,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pmmp\encoding\ByteBufferReader;
 use pmmp\encoding\ByteBufferWriter;
-use pmmp\encoding\VarInt;
-use pocketmine\network\mcpe\protocol\serializer\CommonTypes;
 use pocketmine\network\mcpe\protocol\types\resourcepacks\ResourcePackClientResponse;
-use pocketmine\network\mcpe\protocol\types\resourcepacks\ResourcePackClientResponseType;
 
 class ResourcePackClientResponsePacket extends DataPacket implements ServerboundPacket{
 	public const NETWORK_ID = ProtocolInfo::RESOURCE_PACK_CLIENT_RESPONSE_PACKET;
@@ -40,20 +37,10 @@ class ResourcePackClientResponsePacket extends DataPacket implements Serverbound
 	}
 
 	protected function decodePayload(ByteBufferReader $in) : void{
-		VarInt::readUnsignedInt($in);
-		CommonTypes::getString($in);
-
 		$this->response = ResourcePackClientResponse::read($in);
 	}
 
 	protected function encodePayload(ByteBufferWriter $out) : void{
-		VarInt::writeUnsignedInt($out, $this->response->getType()->value);
-		CommonTypes::putString($out, match($this->response->getType()){
-				ResourcePackClientResponseType::CANCEL => "cancel",
-				ResourcePackClientResponseType::DOWNLOADING => "downloading",
-				ResourcePackClientResponseType::DOWNLOADING_FINISHED => "downloadingfinished",
-				ResourcePackClientResponseType::RESOURCE_PACK_STACK_FINISHED => "resourcepackstackfinished",
-		});
 		$this->response->write($out);
 	}
 
